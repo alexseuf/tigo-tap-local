3f2201f26f84f9b9832fc91017435b4ba4e5a202
alexseuf <48165001+alexseuf@users.noreply.github.com>
2026-09-19 09:24:01 +0200

    Add passive RS485 receiver: custom_components/tigo_tap_local/const.py

--- custom_components/tigo_tap_local/const.py
+++ custom_components/tigo_tap_local/const.py
@@ -1,1 +1,9 @@
 DOMAIN = "tigo_tap_local"
+PLATFORMS = ["sensor"]
+
+CONF_SERIAL_PORT = "serial_port"
+CONF_BAUDRATE = "baudrate"
+DEFAULT_BAUDRATE = 38400
+
+DATA_RECEIVER = "receiver"
+SIGNAL_FRAME = "tigo_tap_local_frame"
